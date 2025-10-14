8f04b06f53dcb913d8ffabc731326b259b104c03
seinlei <seimleihtarni@gmail.com>
2025-10-14 18:07:44 +0700

    Updated Designs

--- scripts/script1.js
+++ scripts/script1.js
@@ -15,26 +15,4 @@ for (var i = 0; i < skillLines.length; i++) {
     });
 }
 
-    const items = document.querySelectorAll('.timeline-item');
-    let currentIndex = 0;
-
-    // Function to show the current education item and hide the rest
-    function showCurrentItem() {
-        items.forEach((item, index) => {
-            if (index === currentIndex) {
-                item.style.display = 'block';
-            } else {
-                item.style.display = 'none';
-            }
-        });
-    }
-
-    // Start the slideshow by showing the first item
-    showCurrentItem();
-
-    // Set an interval to switch items every 5 seconds
-    setInterval(() => {
-        currentIndex = (currentIndex + 1) % items.length;
-        showCurrentItem();
-    }, 5000); // 5000ms = 5 seconds
 
